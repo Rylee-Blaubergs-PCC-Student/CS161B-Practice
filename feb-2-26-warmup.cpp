a3cf422f2e559e503015c5dfca1bfb5bc4d7af2d
Rylee-Blaubergs-PCC-Student <rylee.blaubergs@pcc.edu>
2026-02-02 18:15:24 -0800

    Create feb-2-26-warmup.cpp with initial setup
    Initialize feb-2-26-warmup.cpp with function prototypes and constants.

---
+++ feb-2-26-warmup.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+// function prototypes
+bool setContains(const int set[], const int size, int searchNum);
+
+// declare const
+const int set[20];
+const int size;
+
+// a set is a list of objects in no paticular order, but there cannot be any duplicates
+
+int main() {
+int searchNum = 0;
+
+
+
+
+
+return 0;
+}
